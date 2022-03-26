414f70a6ff706e2b0722e24504585beaf0d4b7f6
konstantgr <60608301+konstantgr@users.noreply.github.com>
2022-03-26 17:07:18 +0300

    fixed algorithm

--- genetic_sample.py
+++ genetic_sample.py
@@ -8,7 +8,6 @@ from geometries import circles_grid, add_circle
 
 class Individual:
     def __init__(self, x, model):
-        self.indices = self.get_indices(x)
         self.model = model
         self.n = int(np.sqrt(len(x)))
 
@@ -17,20 +16,24 @@ class Individual:
         self.selections = model/'selections'
         self.amodel = model/'geometries'/'Geometry 1'
 
-        clean(self.amodel, 'circle')
-        clean(self.selections, 'circle')
-        clean(self.amodel, 'square')
-        clean(self.selections, 'square')
+        self.clean_all()
 
+        self.indices = self.get_indices(x)
         self.initial_params = {
             'xlim': (-0.05, 0.05),
             'ylim': (-0.05, 0.05),
             'n': self.n, 
         }
-        
+
     @staticmethod
     def get_indices(x):
         return np.nonzero(x)
+
+    def clean_all(self):
+        clean(self.amodel, 'circle')
+        clean(self.selections, 'circle')
+        clean(self.amodel, 'square')
+        clean(self.selections, 'square')
 
     def create_model(self):
         node_selections = []
@@ -45,21 +48,16 @@ class Individual:
             for y_j in y:
                 name = f"circle_xi_{x_i}, yj_{y_j}"
 
-                if idx in list(indices[0]):
-                    node, node_sel = add_circle(
-                        name, 
-                        x_i, y_j, 
-                        self.amodel, self.selections, 
-                        radius, alpha
-                    )        
+                if idx in list(self.indices[0]):
+                    node, node_sel = add_circle(name, x_i, y_j, 
+                        self.amodel, self.selections, radius, alpha)        
                     node_selections.append(node_sel)
-
                 else:
                     node_selections.append(None)
 
                 idx += 1
 
-        plastic.property('input', list(np.array(node_selections)[self.indices]))
+        self.plastic.property('input', list(np.array(node_selections)[self.indices]))
         self.model.build(self.amodel)
 
     def solve(self):
@@ -74,15 +72,18 @@ def transform_to_binary(x):
     return [int(x_i > 0.5) for x_i in x]
 
 
-def fitness(x):
+def fitness(x, model):
     x = transform_to_binary(x)
-    ind = Individual(x, model=None)
+    ind = Individual(x, model=model)
     ind.create_model()
     ind.solve()
     return ind.fitness()
 
 
-def simple_genetic(n):
+def simple_genetic(n, model):
     bounds = [(0, 1) for i in range(n**2)]
-    result = differential_evolution(fitness, bounds, seed=1, maxiter=100)
+    result = differential_evolution(
+        lambda x: fitness(x, model), 
+        bounds, seed=1, maxiter=100
+    )
     return result.x, result.fun
